542724966ae90a6667cb919cc0a578b41c99b853
raghavv619 <graghavv@outlook.com>
2025-10-12 22:18:53 +0530

    DB Added

--- src/env.js
+++ src/env.js
@@ -11,6 +11,13 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+
+      SINGLESTORE_USER:z.string(),
+      SINGLESTORE_PASS:z.string(),
+
+      SINGLESTORE_HOST:z.string(),
+      SINGLESTORE_PORT:z.string(),
+      SINGLESTORE_DBNAME:z.string(),
   },
 
   /**
@@ -29,6 +36,11 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
+    SINGLESTORE_USER:process.env.SINGLESTORE_USER,
+    SINGLESTORE_PASS:process.env.SINGLESTORE_PASS,
+    SINGLESTORE_HOST:process.env.SINGLESTORE_HOST,
+    SINGLESTORE_PORT:process.env.SINGLESTORE_PORT,
+    SINGLESTORE_DBNAME:process.env.SINGLESTORE_DBNAME,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
