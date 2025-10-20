25a6819f85c2322dcc7280d359f02d5b1735275f
raghavv619 <graghavv@outlook.com>
2025-10-20 17:38:41 +0530

    Move off client state and se real Links for navigation

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,5 +1,6 @@
 import type { folders, files } from "~/server/db/schema";
 import { Folder as FolderIcon, FileIcon } from "lucide-react";
+import Link from "next/link";
 
 export function FileRow(props: { file: typeof files.$inferSelect }) {
   const { file } = props;
@@ -30,7 +31,7 @@ export function FolderRow(props: {
   folder: typeof folders.$inferSelect;
   handleFolderClick: () => void;
 }) {
-  const { folder, handleFolderClick } = props;
+  const { folder } = props;
   return (
     <li
       key={folder.id}
@@ -38,13 +39,12 @@ export function FolderRow(props: {
     >
       <div className="grid grid-cols-12 items-center gap-4">
         <div className="col-span-6 flex items-center">
-          <button
-            onClick={() => handleFolderClick()}
+          <Link href={`/f/${folder.id}`}
             className="flex items-center text-gray-100 hover:text-blue-400"
           >
             <FolderIcon className="mr-3" size={20} />
             {folder.name}
-          </button>
+          </Link>
         </div>
         <div className="col-span-3 text-gray-400"></div>
         <div className="col-span-3 text-gray-400"></div>
