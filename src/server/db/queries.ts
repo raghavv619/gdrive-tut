e608960f8c7ed5f1968c8c319da8b7e55a1ee3b5
raghavv619 <graghavv@outlook.com>
2025-10-23 13:41:31 +0530

    fix type errors

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -47,10 +47,11 @@ export const MUTATIONS = {
     name:string;
     size:number;
     url:string;
+    parent:number;
   };
 userId:string;}) {
     return await db.insert(filesSchema).values({...input.file,
-      parent:1,}
+      parent:input.file.parent,}
     )
   }
 }
